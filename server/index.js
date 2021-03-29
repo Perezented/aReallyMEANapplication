4af00945e3e7095ac4c6e9886dfb29cba4f08c1d
Michael Perez <mperez6473@gmail.com>
2021-03-29 14:33:08 -0500

    passport initialized in main index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const passport = require("passport");
 const mongoose = require("mongoose");
 const config = require("./config/database");
 
@@ -31,6 +31,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./config/passport")(passport);
 
 app.use("/users", users);
 
