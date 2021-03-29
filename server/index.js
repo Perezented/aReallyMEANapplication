5629d6e98515f32e6289b0b3bd3128de7415deb1
Michael Perez <mperez6473@gmail.com>
2021-03-29 14:11:46 -0500

    Have mongoose started to get connection to the db

--- server/index.js
+++ server/index.js
@@ -3,6 +3,20 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+// Connect to DB
+mongoose.connect(config.database);
+// On Connection, display message
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database " + config.database);
+});
+// On error
+mongoose.connection.on("error", (err) => {
+  console.log("DB connection error " + err);
+});
+
 const app = express();
 
 const users = require("./routes/users");
