8eeb95684660c899dd3a4e3996871f68de3c919e
Michael Perez <mperez6473@gmail.com>
2021-03-29 14:22:37 -0500

    Ability to register users. Typo fix on auth post

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users.js");
 
 // get to users
 router.get("/", (req, res) => {
@@ -7,13 +8,25 @@ router.get("/", (req, res) => {
 });
 
 // Register
+router.post("/register", (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
 
-router.get("/register", (req, res) => {
-  res.send("Register page");
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ success: false, msg: "Failed to register user" });
+    } else {
+      res.json({ success: true, msg: "User registered." });
+    }
+  });
 });
 
 // Authentication
-router.get("/authenticate", (req, res) => {
+router.post("/authenticate", (req, res) => {
   res.send("Authenticate page");
 });
 
