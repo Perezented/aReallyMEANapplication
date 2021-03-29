d3fdbbb0ad4ccca8e49ed2a4098a1884e12f2b24
Michael Perez <mperez6473@gmail.com>
2021-03-29 10:38:27 -0500

    middlewares added as well as routes to users. Public folder being used for displaying on root url

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
+const users = require("./routes/users");
+
 const port = 3007;
 
+// CORS Middleware
+app.use(cors());
+
+// Setting static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use("/users", users);
+
+// main index route
 app.get("/", (req, res) => {
   res.send(
     "You got to the / of this local host page. Welcome to the empty home page."
   );
 });
-
+// Message for server start
 app.listen(port, () => {
   console.log("Server started on localhost:" + port);
 });
