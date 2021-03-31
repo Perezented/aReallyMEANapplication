490e3e5f7a0e4b1ec209ee250a2a3bca6aa5cc6b
Michael Perez <mperez6473@gmail.com>
2021-03-31 14:49:35 -0500

    console.log removal

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -22,7 +22,6 @@ export class LoginComponent implements OnInit {
 
 
   onLoginSubmit() {
-    console.log(this.username)
     const user = {
       username: this.username,
       password: this.password
